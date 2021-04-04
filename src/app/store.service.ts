00c283384fbd9d6805f2be58f5306c728db1a26c
LucasViniciusLima <lucaslimavzt@gmail.com>
2021-04-04 12:40:30 -0300

    retorna um pedido

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -9,19 +9,31 @@ export class StoreService {
 
   requestsRef = this.store.collection('pedido');
   requestList: Array<any> = [];
-
+  
   constructor(private store: AngularFirestore) {
-    
+    console.log(this.getOneRequest("1TXqP6fTaMvA0IxtdISB"));
   }
 
-  addRequest(){
+  addRequest() {
     //this.store.collection('pedido').add({obj});
   }
-  completeRequestById(id: string){
-    this.requestsRef.doc(id).update({"finalizado": true});
+  changeRequestStatus(id: string) {
+    this.requestsRef.doc(id).update({ "finalizado": true });
   }
-  getAllRequests(){
-    this.requestsRef.get().toPromise().then((querySnapshot) => { querySnapshot.forEach((doc) => {this.requestList.push(doc.data());});});
+  getAllRequests() {
+    this.requestsRef.get().toPromise().then((querySnapshot) => { querySnapshot.forEach((doc) => { this.requestList.push(doc.data()); }); });
   }
+  getOneRequest(id: string) {
+    this.requestsRef.doc(id).get().toPromise().then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        return doc.data();
+      } else { console.log("No such document!"); }
+    }).catch((error) => {
+      console.log("Error getting document:", error);
+    });
+    return null;
+  }
+
 }
 
