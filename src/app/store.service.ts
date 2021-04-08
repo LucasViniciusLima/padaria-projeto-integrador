5804fbea293837f19c64172610be972cbd8c5c57
LucasViniciusLima <lucaslimavzt@gmail.com>
2021-04-07 23:08:48 -0300

    user returns with name

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -2,7 +2,7 @@ import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map } from "rxjs/operators"; 
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -12,31 +12,51 @@ export class StoreService {
   tasksCollection: AngularFirestoreCollection<any>;
   userDoc: AngularFirestoreDocument<any>;
   tasks: Observable<any[]>;
-  
+
+
   constructor(private store: AngularFirestore) {
     this.tasksCollection = this.store.collection('pedido', ref => ref.orderBy('data').limit(30));
-        
-    this.tasks = this.tasksCollection.snapshotChanges().pipe(map(changes => {
-      return changes.map(a => {
-        const data = a.payload.doc.data() as any;
-        data.id = a.payload.doc.id;
-        return data;
-      });
-    }));
+    //tente ao maximo nomear isso certo
+    this.tasks = this.tasksCollection.snapshotChanges().pipe(
+      map(tarefasCollection => {
+        return tarefasCollection.map(a => {
+          const data = a.payload.doc.data() as any;
+          data.id = a.payload.doc.id;
+          return data;
+        });
+      }),
+      map(tarefas => {
+        tarefas.map(item => {
+          this.getSchedule(item.idCliente).then(data => {
+            var cliente = data as any;
+            item.nomeCliente = cliente.nome;
+          });
+        })
+        console.table(tarefas);
+        return tarefas;
+      })
+    );
+    console.table(this.tasks);
   }
-  
-  getItems(){
+
+  getItems() {
     return this.tasks;
   }
 
   completeRequest(id: string, newStatus: boolean) {
-    this.store.collection('pedido').doc(id).update({ "finalizado": newStatus});
+    this.store.collection('pedido').doc(id).update({ "finalizado": newStatus });
   }
 
-  getUser(id: string){
-    this.userDoc = this.store.collection('usuario').doc(id);
-    return this.userDoc.valueChanges();
+  getSchedule(id: string) {
+    return this.store
+      .collection("usuario")
+      .doc(id)
+      .ref.get()
+      .then(function (doc) {
+        return doc.data();
+      });
   }
-  
+
+
 }
 
